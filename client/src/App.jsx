648a8008e97fab23e7574c0f7ce9aca95de04b4c
Xavier <xavier@localhost>
2024-07-12 23:57:51 +0200

    Wagmi + WalletConnect setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,38 @@ import { ToastProvider } from './Providers';
 import Toast from './components/ui/Toast';
 import { router } from './routes';
 
+import { createWeb3Modal } from '@web3modal/wagmi/react';
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
+
+import { WagmiProvider } from 'wagmi';
+import { mainnet, arbitrum, goerli, scrollSepolia, celoAlfajores } from 'wagmi/chains';
+
+// 1. Get projectId from https://cloud.walletconnect.com
+const projectId = 'adad6ddb068edeb3c80dccb1bf3e4673';
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: 'Tok2Tok',
+  description: 'Tok2Tok',
+  url: 'https://tok2tok.ai',
+  icons: ['https://avatars.githubusercontent.com/u/37784886'],
+};
+
+const chains = [mainnet, arbitrum, goerli, scrollSepolia, celoAlfajores];
+const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+});
+
+// 3. Create modal
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true, // Optional - false as default
+});
+
 const App = () => {
   const { setError } = useApiErrorBoundary();
 
@@ -24,27 +56,29 @@ const App = () => {
   });
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <ThemeProvider>
-          <RadixToast.Provider>
-            <ToastProvider>
-              <DndProvider backend={HTML5Backend}>
-                <RouterProvider router={router} />
-                <ReactQueryDevtools initialIsOpen={false} position="top-right" />
-                <Toast />
-                <RadixToast.Viewport className="pointer-events-none fixed inset-0 z-[1000] mx-auto my-2 flex max-w-[560px] flex-col items-stretch justify-start md:pb-5" />
-              </DndProvider>
-            </ToastProvider>
-          </RadixToast.Provider>
-        </ThemeProvider>
-      </RecoilRoot>
-    </QueryClientProvider>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <ThemeProvider>
+            <RadixToast.Provider>
+              <ToastProvider>
+                <DndProvider backend={HTML5Backend}>
+                  <RouterProvider router={router} />
+                  <ReactQueryDevtools initialIsOpen={false} position="top-right" />
+                  <Toast />
+                  <RadixToast.Viewport className="pointer-events-none fixed inset-0 z-[1000] mx-auto my-2 flex max-w-[560px] flex-col items-stretch justify-start md:pb-5" />
+                </DndProvider>
+              </ToastProvider>
+            </RadixToast.Provider>
+          </ThemeProvider>
+        </RecoilRoot>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 };
 
 export default () => (
   <ScreenshotProvider>
-    <App />
+    <App/>
   </ScreenshotProvider>
 );
