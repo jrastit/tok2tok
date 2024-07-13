05d6be2a3dad39d6e3239f83aca0bcca0f0d9201
Xavier <xavier@localhost>
2024-07-13 22:07:49 +0200

    Mint 100 USDIA

--- client/src/components/Nav/MintButton.tsx
+++ client/src/components/Nav/MintButton.tsx
@@ -1,6 +1,8 @@
 import USDIA_test from '../../../../tok2tok-contract/contracts/artifacts/USDIA_test.json';
 import { useAccount, useSimulateContract, useWriteContract } from 'wagmi';
 
+const amountToMint = 100n;
+
 const MintButton = () => {
   const { address } = useAccount();
 
@@ -8,7 +10,7 @@ const MintButton = () => {
     address: '0x0e87Fe746789dAb39B98a3E8c44D38665Ed55952',
     abi: USDIA_test.abi,
     functionName: 'mint',
-    args: [address, 1000000000000000000n],
+    args: [address, amountToMint * 1000000000000000000n],
   });
   const { writeContractAsync } = useWriteContract();
 
@@ -20,7 +22,7 @@ const MintButton = () => {
         console.log(`##### Mint transaction id=${transactionId}`);
       }}
     >
-      Mint 1 USDC
+      Mint {amountToMint.toString()} USDC
     </button>
   );
 };
