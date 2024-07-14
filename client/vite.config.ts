eb3b500e896338ea4745ee78f19372b51c69b128
Xavier <xavier@localhost>
2024-07-14 04:08:16 +0200

    Fix page title

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -64,8 +64,8 @@ export default defineConfig({
         maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
       },
       manifest: {
-        name: 'LibreChat',
-        short_name: 'LibreChat',
+        name: 'Tok2Tok IA',
+        short_name: 'Tok2Tok',
         start_url: '/',
         display: 'standalone',
         background_color: '#000000',
